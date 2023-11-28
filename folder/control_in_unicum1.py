4825c881d230df73eed65fb2a21b362742c020ac
KMS1437 <116000632+KMS1437@users.noreply.github.com>
2023-11-28 09:34:20 +0700

    message is not i

---
+++ folder/control_in_unicum1.py
@@ -0,0 +1,173 @@
+# 1-е задание
+
+import random
+
+list_length = int(input("Введите длину списков:"))
+
+list1 = [random.randint(1, 10) for i in range(list_length)]
+list2 = [random.randint(1, 10) for i in range(list_length)]
+
+print("Список 1:", list1)
+print("Список 2:", list2)
+
+sorted_list = sorted(list1 + list2)
+
+print("Отсортированный список:", sorted_list)
+
+unique_elements = set(list1).intersection(set(list2))
+
+print("Уникальные числа:", list(unique_elements))
+
+max_value = max(list1 + list2)
+min_value = min(list1 + list2)
+
+print("Минимальное значение числа:", min_value)
+print("Максимальное значение числа:", max_value)
+
+# 2-е задание
+
+phone_brands = ('Orange', 'Siaoni', 'Sinsumg', 'Hiavei')
+
+def print_menu():
+    print("1. Добавить новый бренд")
+    print("2. Удалить бренд")
+    print("3. Показать список брендов")
+    print("4. Выход из программы")
+
+def add_brand(brand, brands):
+    if brand in brands:
+        print("Бренд уже существует!")
+    else:
+        brands += (brand,)
+        print("Теперь ваш новый бренд существует!")
+    return brands
+
+def remove_brand(brand, brands):
+    if brand not in brands:
+        print("Бренд не найден! Попробуй ещё раз")
+    else:
+        brands = tuple(x for x in brands if x != brand)
+        print("Бренд удален! Можно отпразновать!")
+    return brands
+
+print("Список брендов:", phone_brands)
+
+while True:
+    print_menu()
+    choice = input("Выберите действие (введите цифру действия):")
+
+    if choice == '1':
+        brand = input("Введите название бренда: ")
+        phone_brands = add_brand(brand, phone_brands)
+        print("Список брендов:", phone_brands)
+    elif choice == '2':
+        brand = input("Введите название бренда: ")
+        phone_brands = remove_brand(brand, phone_brands)
+        print("Список брендов:", phone_brands)
+    elif choice == '3':
+        print("Список брендов:", phone_brands)
+    elif choice == '4':
+        break
+    else:
+        print("Неправильно! Попробуй, ещё раз!")
+
+# 3-е задание
+
+countries = set(['Россия', 'США', 'Беларусь', 'Египет', 'Таджикистан'])
+
+while True:
+
+    print('1. Посмотреть список стран')
+    print('2. Добавить свою страну')
+    print('3. Удалить страну')
+    print('4. Найти страну')
+    print('5. Выйти из программы')
+
+    choice = input('Введите номер действия: ')
+
+    if choice == '1':
+        print(countries)
+
+    elif choice == '2':
+        your_country = input('Введите свою страну: ')
+        countries.add(your_country)
+        print(f'Страна {your_country} добавлена в множество.')
+
+    elif choice == '3':
+        del_country = input('Введите название страны, которую нужно удалить: ')
+        if del_country in countries:
+            countries.remove(del_country)
+            print(f'Страна {del_country} удалена')
+        else:
+            print(f'Страны {del_country} нет в программе')
+
+    elif choice == '4':
+        find_country = input('Введите название страны для поиска: ')
+        if find_country in countries:
+            print(f'Страна {find_country} найдена')
+        else:
+            print(f'Страна {find_country} не найдена')
+
+    elif choice == '5':
+        break
+
+    else:
+        print('Неправильно ввёл. Переделывай')
+
+# 4-е задание
+
+dictionary = {'Муджахед': 1871, 'Игорёк Бобылев': 2010, 'Мишань Камозин': 2009}
+
+while True:
+    print('Что вы хотите сделать?')
+    print('1 - Добавить новый элемент в словарь')
+    print('2 - Удалить элемент из словаря')
+    print('3 - Изменить элемент в словаре')
+    print('4 - Вывести текущий словарь')
+    print('5 - Выйти из программы')
+
+    choice = input()
+
+    if choice == '1':
+        name = input('Введите свой имя: ')
+        year_of_birth = input('Введите свой год рождения: ')
+
+        try:
+            year_of_birth = int(year_of_birth)
+            dictionary[name] = year_of_birth
+            print('Элемент добавлен в словарь.')
+        except ValueError:
+            print('Ошибка! Год рождения должен быть числом.')
+
+    elif choice == '2':
+        name = input('Введите имя: ')
+
+        if name in dictionary:
+            del dictionary[name]
+            print('Элемент удалён из словаря.')
+        else:
+            print('Ошибка! Элемент с таким именем не найден.')
+
+    elif choice == '3':
+        name = input('Введите имя: ')
+
+        if name in dictionary:
+            year_of_birth = input("Введите новый год рождения:")
+
+            try:
+                year_of_birth = int(year_of_birth)
+                dictionary[name] = year_of_birth
+                print("Элемент изменён в словаре.")
+            except ValueError:
+                print("Ошибка! Год рождения должен быть числом.")
+        else:
+            print("Ошибка! Элемент с таким именем не найден.")
+
+    elif choice == '4':
+        print(dictionary)
+
+    elif choice == '5':
+        break
+
+    else:
+        print("Неправильно! Попробуй ещё раз!")
