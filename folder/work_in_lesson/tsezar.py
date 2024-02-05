c72bd4a8ff492fd6c669b930f11e7ef9aee7e629
Михаил <116000632+KMS1437@users.noreply.github.com>
2024-02-05 19:52:53 +0700

    Update tsezar.py

--- folder/work_in_lesson/tsezar.py
+++ folder/work_in_lesson/tsezar.py
@@ -1,46 +1,59 @@
-alfavit_EU =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alfavit_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-offset = int(input('Шаг шифровки: '))
-message = input("Сообщение для шифровки: ").upper()
-result = ''
-lang = input('Выберите язык RU/EU: ')
-if lang == 'RU':
-    for i in message:
-        place = alfavit_RU.find(i)
-        new_place = place + offset
-        if i in alfavit_RU:
-            result += alfavit_RU[new_place]
-        else:
-            result += i
-else:
-    for i in message:
-        place = alfavit_EU.find(i)
-        new_place = place + offset
-        if i in alfavit_EU:
-            result += alfavit_EU[new_place]
-        else:
-            result += i
-print (result)
+while True:
+    a = int(input('Выберите операцию:\n1. Шифровка\n2. Дешифровка\n3. Выход\nВведите число: '))
 
-alfavit_EU =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-alfavit_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
-offset = int(input('Шаг шифровки: '))
-message = input("Сообщение для ДЕшифровки: ").upper()
-result2 = ''
-if lang == 'RU':
-    for i in message:
-        place = alfavit_RU.find(i)
-        new_place = place - offset
-        if i in alfavit_RU:
-            result2 += alfavit_RU[new_place]
+    if a == 1:
+        alfavit_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        alfavit_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+        offset = int(input('Шаг шифровки: '))
+        message = input("Сообщение для шифровки: ").upper()
+        result = ''
+        lang = input('Выберите язык RU/EU: ')
+        if lang == 'RU':
+            for i in message:
+                if i in alfavit_RU:
+                    place = alfavit_RU.find(i)
+                    new_place = (place + offset) % len(alfavit_RU)
+                    result += alfavit_RU[new_place]
+                else:
+                    result += i
         else:
-            result2 += i
-else:
-    for i in message:
-        place = alfavit_EU.find(i)
-        new_place = place - offset
-        if i in alfavit_EU:
-            result2 += alfavit_EU[new_place]
+            for i in message:
+                if i in alfavit_EU:
+                    place = alfavit_EU.find(i)
+                    new_place = (place + offset) % len(alfavit_EU)
+                    result += alfavit_EU[new_place]
+                else:
+                    result += i
+        print(result)
+
+    elif a == 2:
+        alfavit_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        alfavit_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+        offset = int(input('Шаг дешифровки: '))
+        message = input("Сообщение для дешифровки: ").upper()
+        result2 = ''
+        lang = input('Выберите язык RU/EU: ')
+        if lang == 'RU':
+            for i in message:
+                if i in alfavit_RU:
+                    place = alfavit_RU.find(i)
+                    new_place = (place - offset) % len(alfavit_RU)
+                    result2 += alfavit_RU[new_place]
+                else:
+                    result2 += i
         else:
-            result2 += i
-print(result2)
+            for i in message:
+                if i in alfavit_EU:
+                    place = alfavit_EU.find(i)
+                    new_place = (place - offset) % len(alfavit_EU)
+                    result2 += alfavit_EU[new_place]
+                else:
+                    result2 += i
+        print(result2)
+
+    elif a == 3:
+        print("До свидания!")
+        break
+    
+    else:
+        print("Неверный выбор операции. Пожалуйста, выберите 1 для шифровки, 2 для дешифровки или 3 для выхода.")
