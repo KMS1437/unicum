07eeffb003b0c0a5dd102392235de1c0b54c57cc
Михаил <116000632+KMS1437@users.noreply.github.com>
2024-04-09 22:03:18 +0700

    Update main.py

--- projects/algebric/main.py
+++ projects/algebric/main.py
@@ -1,9 +1,10 @@
 from telebot import types
+import random
 import telebot
 import re
 from sympy import sympify, expand
 
-bot = telebot.TeleBot("Максим тут нет токена))")
+bot = telebot.TeleBot("Токена нема((")
 
 def replace_superscript(text):
     superscripts = {
@@ -46,12 +47,15 @@ def handle_semen(message):
 @bot.message_handler(func=lambda message: True)
 def handle_text(message):
     user_input = message.text
+    chance = random.randrange(1, 100)
     if message.from_user.id == 5041299186:
-        bot.send_message(message.chat.id, f"*⚡ Пошел лесом, {message.from_user.first_name}*", parse_mode="Markdown")
+        bot.send_message(message.chat.id, f"*⚡ Тебе тут не место, {message.from_user.first_name}. Возможно, вы делали непозволительные вещи с ботом*", parse_mode="Markdown")
     elif user_input == "🏆 Миша испорченный до невозможности":
         bot.send_message(message.chat.id, f"*️⚜ Однозначно, {message.from_user.first_name}!*", parse_mode="Markdown")
+    elif user_input == "💀 Насколько испорченный я?":
+        bot.send_message(message.chat.id, f"*⚜ {message.from_user.first_name}, вы испорченны на {chance}%*", parse_mode="Markdown")
     elif user_input == "💻 Как пользоваться этим ботом?":
-        bot.send_message(message.chat.id, f"*️⚜ {message.from_user.first_name}, напишите любой многочлен или алгеброическое выражение, к примеру (a-3)*(a+3).*", parse_mode="Markdown")
+        bot.send_message(message.chat.id, f"*⚜ {message.from_user.first_name}, напишите любой многочлен или алгеброическое выражение, к примеру (a-3)(a+3).*", parse_mode="Markdown")
         bot.send_message(message.chat.id, "* Чтобы бот решил все, степень нужно указывать через ^, а умножение обозначается звездочкой, дробь в свою очередь обозначается как /, это же и деление. *", reply_markup=get_keyboard(), parse_mode="Markdown")
     elif message.text == "❓ Обновления проекта":
         markup = telebot.types.InlineKeyboardMarkup()
@@ -74,8 +78,9 @@ def get_keyboard():
     markup = types.ReplyKeyboardMarkup(row_width=2)
     button1 = types.KeyboardButton("🏆 Миша испорченный до невозможности")
     button2 = types.KeyboardButton("💻 Как пользоваться этим ботом?")
-    button3 = types.KeyboardButton("❓ Обновления проекта")
-    markup.add(button1, button2, button3)
+    button3 = types.KeyboardButton("💀 Насколько испорченный я?")
+    button4 = types.KeyboardButton("❓ Обновления проекта")
+    markup.add(button1, button2, button3, button4)
     return markup
 
 if __name__ == "__main__":
